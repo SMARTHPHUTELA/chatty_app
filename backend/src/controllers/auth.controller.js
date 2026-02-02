8c603686b6acf52fd6340c8da3804c5cb8da5d27
Smarth Phutela <smarthphutela9@gmail.com>
2026-02-03 03:01:07 +0530

    message routes,model,controller frontend initial setup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
@@ -84,6 +85,33 @@ export const logout=(req,res)=>{
         res.status(500).json({message:"Internal Server error"});
     }
 }
-export const updateProfile=(req,res)=>{
-    
+export const updateProfile=async (req,res)=>{
+    try {
+        const profilePic= req.body;
+        const userId=req.user._id;
+        if(!profilePic){
+            return res.status(400).json({message:"Profile Pic is Required"});
+        }
+
+        const uploadedResponse =await cloudinary.uploader.upload(profilePic);
+        const updatedUser=await User.findByIdAndUpdate(
+            userId,
+            {profilePic:uploadedResponse.secure_url},
+            {new:true}
+        );
+        return res.status(200).json(updatedUser)
+    } catch (error) {
+        console.log("Error in Update Profile",error.message);
+        return res.status(500).json({message: "Internal Server Error"});
+        
+    }
+}
+
+export const checkAuth=async(req,res)=>{
+    try {
+        return res.status(200).json(req.user);
+    } catch (error) {
+        console.log("User not authenticated");
+        return res.status(500).json({message:"Internal Server Error"});
+    }
 }
