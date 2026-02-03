71077eb5d77f26f161cd8f57c2632c0e243eb09e
Smarth Phutela <smarthphutela9@gmail.com>
2026-02-04 02:00:28 +0530

    zustand ,axios set Up cors bypass

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 dotenv.config()
@@ -10,6 +11,10 @@ dotenv.config()
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}));
 const PORT=process.env.PORT;
 
 app.use("/api/auth",authRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import { Navigate, Routes } from 'react-router'
+import { Route } from 'react-router-dom'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
+import SettingPage from './pages/SettingPage'
+import { useAuthStore } from './store/useAuthStore'
+import {Loader} from "lucide-react";
+
 
 const App = () => {
+  const{authUser,checkAuth,isCheckingAuth}=useAuthStore();
+
+  useEffect(()=>{
+    checkAuth();
+  },[checkAuth]);
+
+  console.log(authUser);
+  if(isCheckingAuth && !authUser){
+    return(
+      <div className="flex items-center justify-center h-screen">
+        <Loader className='size-10 animate-spin'/>
+      </div>
+    )
+  }
+
   return (
-    <div className='text-red-600 text-7xl'>
-      hello
-    </div>
+    <>
+      <Navbar/>
+
+      <Routes>
+        <Route path='/signup' element={<SignUpPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/' element={authUser ? <HomePage/> : <Navigate to={"/login"}/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+        <Route path='/setting' element={<SettingPage/>}/>
+      </Routes>
+    </>
   )
 }
 
